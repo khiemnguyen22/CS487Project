33cb9a101011fbf2f405b0a01ec2b9abac0c0b2d
khiemnguyen22 <55027703+khiemnguyen22@users.noreply.github.com>
2021-04-29 17:35:22 +0700

    created User class, added 2 text boxes and 1 button for login

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,10 +14,12 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
