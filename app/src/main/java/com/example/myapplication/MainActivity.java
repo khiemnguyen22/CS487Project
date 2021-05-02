414707380ca31e29491f43ab88f2e5442ef73c8d
khiemnguyen22 <55027703+khiemnguyen22@users.noreply.github.com>
2021-05-02 08:45:20 +0700

    Revert "Implemented initial Login page"
    This reverts commit d772406e933ffac31e9da5b7b4e96c9c381bf714.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,84 +1,60 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+// checking github commit
+// checking push
 
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.TextPaint;
-import android.text.method.LinkMovementMethod;
-import android.text.style.ClickableSpan;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    @Override
-    // TODO: add Onclick to buttons
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        TextView forgotTextView = findViewById(R.id.forgotUsername);
-        TextView createAccountView = findViewById(R.id.firstTime);
-
-        String forgotText = "Forgot Username? Forgot Password? ";
-        String createAccount = "First time? Create an Account";
-
-        SpannableString ss1 = new SpannableString(forgotText);
-        SpannableString ss2 = new SpannableString(createAccount);
-
-        ClickableSpan cs1 = new ClickableSpan() {
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(@NonNull View widget) {
-                Toast.makeText(MainActivity.this, "Forgot username", Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
-            public void updateDrawState(@NonNull TextPaint ds) {
-                super.updateDrawState(ds);
-                ds.setColor(Color.BLUE);
-            }
-        };
-        ClickableSpan cs2 = new ClickableSpan() {
-            @Override
-            public void onClick(@NonNull View widget) {
-                Toast.makeText(MainActivity.this, "Forgot password", Toast.LENGTH_SHORT).show();
-            }
-            public void updateDrawState(@NonNull TextPaint ds) {
-                super.updateDrawState(ds);
-                ds.setColor(Color.BLUE);
-            }
-        };
-        ClickableSpan cs3 = new ClickableSpan() {
-            @Override
-            public void onClick(@NonNull View widget) {
-                Toast.makeText(MainActivity.this, "Create an account", Toast.LENGTH_SHORT).show();
-
-            }
-
-            @Override
-            public void updateDrawState(@NonNull TextPaint ds) {
-                super.updateDrawState(ds);
-                ds.setColor(Color.BLUE);
-            }
-        };
-        ss1.setSpan(cs1,0,16, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        ss1.setSpan(cs2,17,33, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        ss2.setSpan(cs3,12,29, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-        forgotTextView.setText(ss1);
-        forgotTextView.setMovementMethod(LinkMovementMethod.getInstance());
-
-        createAccountView.setText(ss2);
-        createAccountView.setMovementMethod(LinkMovementMethod.getInstance());
+        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
