e4ab3128aa41c65ded904a5b2c2eb88a031d6dae
khiemnguyen22 <55027703+khiemnguyen22@users.noreply.github.com>
2021-05-04 08:34:16 +0700

    updated EmailUpdated functionality

--- app/src/main/java/com/example/myapplication/UpdateEmail.java
+++ app/src/main/java/com/example/myapplication/UpdateEmail.java
@@ -7,12 +7,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class UpdateEmail extends AppCompatActivity {
 
     EditText usernewEmail, userconfirmEmail;
     Button updatebtn;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +29,20 @@ public class UpdateEmail extends AppCompatActivity {
         usernewEmail = findViewById(R.id.email_conf1);
         userconfirmEmail = findViewById(R.id.email_conf2);
         updatebtn = findViewById(R.id.updatebtn);
+        user = FirebaseAuth.getInstance().getCurrentUser();
 
 
         updatebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(usernewEmail.getText().toString().isEmpty()){
-                    usernewEmail.setError("Required to update Credit Card");
+                    usernewEmail.setError("Required to update Email");
                 }
                 if(userconfirmEmail.getText().toString().isEmpty()){
-                    userconfirmEmail.setError("Required to update Credit Card");
+                    userconfirmEmail.setError("Required to update Email");
                 }
                 if(!usernewEmail.getText().toString().equals(userconfirmEmail.getText().toString())){
-                    userconfirmEmail.setError("Credit Card Number Does Not Match");
+                    userconfirmEmail.setError("Email Does Not Match");
                 }
 
                 DatabaseHelper db = new DatabaseHelper(UpdateEmail.this, 3);
@@ -47,6 +55,20 @@ public class UpdateEmail extends AppCompatActivity {
                 else{
                     Toast.makeText(UpdateEmail.this, "Update Email Failed", Toast.LENGTH_SHORT).show();
                 }
+
+                user.updateEmail(usernewEmail.getText().toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(UpdateEmail.this, "Email Updated",Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(UpdateEmail.this,e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
 
             }
         });
