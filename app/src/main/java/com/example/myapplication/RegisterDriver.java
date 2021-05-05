0e38aeca0655d53bd603c34c4b9f3ae28dc4af8f
William <whaddad@hawk.iit.edu>
2021-05-05 00:35:18 -0400

    Maps activity

--- app/src/main/java/com/example/myapplication/RegisterDriver.java
+++ app/src/main/java/com/example/myapplication/RegisterDriver.java
@@ -1,8 +1,5 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,9 +7,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.AuthResult;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterDriver extends AppCompatActivity {
